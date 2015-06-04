5f0c9e8dba854d0fe19475a9961d688c260656dd
lutherism <xanderjanz@gmail.com>
2015-06-04 02:35:13 -0700

    add splash

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -10,44 +10,66 @@ var Signup = React.createClass({
   },
   render: function() {
     return (
-      <form
-        onSubmit={this.handleSubmit}>
-        <div>
-          <label for="name">
-            Name
-          </label>
-          <input type="text"
-            value={this.state.name}
-            onChange={this.handleInputChange.bind(this, "name")} />
+      <div style={{
+          width: "100%",
+          height: "100%"
+        }}>
+        <div className="splash">
+          <h1>A Chess app from a guy who cares about chess.</h1>
+          <br/>
+          <h2>
+            <p>Features:</p>
+            <br/>
+            <small>
+              <ul>
+                <li>Usable UI.</li>
+                <li>Play unlimited games at once.</li>
+                <li>Find human opponents.</li>
+                <li>ELO Ratings.</li>
+                <li>Free.</li>
+              </ul>
+            </small>
+          </h2>
         </div>
-        <div>
-          <label for="password">
-            Password
-          </label>
-          <input type="password"
-            value={this.state.password}
-            onChange={this.handleInputChange.bind(this, "password")} />
-        </div>
-        <div>
-          <label for="isSignup">
-            Create New Account
-          </label>
-          <input type="checkbox"
-            value={this.state.isSignup}
-            onChange={this.handleInputChange.bind(this, "isSignup")}/>
-        </div>
-        <h3>
-          Optional
-        </h3>
-        <div>
-          <label for="email">Email</label>
-          <input type="text"
-            value={this.state.text}
-            onChange={this.handleInputChange.bind(this, "email")} />
-        </div>
-        <button
-          type="submit">Submit</button>
-      </form>
+        <form
+          onSubmit={this.handleSubmit}>
+          <div className="form-field">
+            <span for="name">
+              Name
+            </span>
+            <input type="text"
+              value={this.state.name}
+              onChange={this.handleInputChange.bind(this, "name")} />
+          </div>
+          <div className="form-field">
+            <span for="password">
+              Password
+            </span>
+            <input type="password"
+              value={this.state.password}
+              onChange={this.handleInputChange.bind(this, "password")} />
+          </div>
+          <div className="form-field">
+            <span for="isSignup">
+              New Account
+            </span>
+            <input type="checkbox"
+              value={this.state.isSignup}
+              onChange={this.handleInputChange.bind(this, "isSignup")}/>
+          </div>
+          <h3>
+            Optional
+          </h3>
+          <div className="form-field">
+            <span for="email">Email</span>
+            <input type="text"
+              value={this.state.text}
+              onChange={this.handleInputChange.bind(this, "email")} />
+          </div>
+          <button
+            type="submit">Submit</button>
+        </form>
+      </div>
     )
   },
   handleInputChange: function(field, e) {
